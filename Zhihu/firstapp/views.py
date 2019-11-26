0915e6d9d103a4469556d4f096406e796e362f25
shawnchen <892230084@qq.com>
2019-11-26 11:37:09 +0800

    Create first_web.html which contains article information

--- Zhihu/firstapp/views.py
+++ Zhihu/firstapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from firstapp.models import People, Article
+
+def index(request):
+    context = {}
+    article_list = Article.objects.all()
+    context['article_list'] = article_list
+    index_page = render(request, 'first_web.html', context)
+    return index_page
